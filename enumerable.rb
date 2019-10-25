afa24ebe08175e4fec7936bbaacd47a2b6794081
Feber <cochabambinoski@gmail.com>
2019-10-24 21:22:20 -0400

    adding my select function

--- enumerable.rb
+++ enumerable.rb
@@ -1,18 +1,29 @@
+# frozen_string_literal: true
+
 module Enumerable
   def my_each
     i = 0
-    while i < self.size
-        yield(self[i])
-        i += 1
+    while i < size
+      yield(self[i])
+      i += 1
     end
   end
 
   def my_each_with_index
     i = 0
-    while i < self.size
-        yield(self[i], i)
-        i += 1
-    end        
+    while i < size
+      yield(self[i], i)
+      i += 1
+    end
   end
-  
+
+  def my_all?
+    i = 0
+    while i < size
+      return false unless yield(self[i])
+
+      i += 1
+    end
+    true
+  end
 end
