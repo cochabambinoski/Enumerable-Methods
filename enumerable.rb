f7b96feb0dac7ff2c7d7b5261f80893f3f1361bb
Feber <cochabambinoski@gmail.com>
2019-10-24 21:16:17 -0400

    adding my each function with index

--- enumerable.rb
+++ enumerable.rb
@@ -5,5 +5,14 @@ module Enumerable
         yield(self[i])
         i += 1
     end
-end
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < self.size
+        yield(self[i], i)
+        i += 1
+    end        
+  end
+  
 end
